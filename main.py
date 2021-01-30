47ab0506692f46e0bec9346330f4a0421ce6d6a6
Axel Uhlig <axel.uhlig@gmail.com>
2021-01-30 20:12:11 +0000

    next steps notes

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ import pickle
 
 from simulator import core
 
+# next steps
+# - return [t,q_l] instead
+# - do n times for every flakiness = [0.01, 0.02, 0.03]
+# - plot data
+
 
 def main():
     sim = core.Simulator()
-    sim.do_basic_sim(initial_queue_length= 5, flakiness= 0.5, new_elements_per_time_interval= 10, cycle= 1000)
+    print(sim.do_basic_sim(initial_queue_length=5, flakiness=0.01,
+                           new_elements_per_time_interval=10, cycle=1000, verbosity=False))
 
 
 if __name__ == "__main__":
