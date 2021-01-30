ca1faf0c46529d68539e7b0d935f4c124fad2abd
Axel Uhlig <axel.uhlig@gmail.com>
2021-01-30 20:21:33 +0000

    go from here

--- main.py
+++ main.py
@@ -5,15 +5,14 @@ import pickle
 from simulator import core
 
 # next steps
-# - return [t,q_l] instead
 # - do n times for every flakiness = [0.01, 0.02, 0.03]
 # - plot data
 
 
 def main():
     sim = core.Simulator()
-    print(sim.do_basic_sim(flakiness=0.01,
-                           new_elements_per_time_interval=10, cycle=1000, verbosity=False))
+    results = sim.do_basic_sim(flakiness=0.01,
+                               new_elements_per_time_interval=10, cycle=1000, verbosity=False)
 
 
 if __name__ == "__main__":
