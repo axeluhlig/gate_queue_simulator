038bfa0ec0da9e2879d442304e9b8c455af5bc45
Axel Uhlig <axel.uhlig@gmail.com>
2021-01-30 18:37:25 +0000

    test green for chances_for_no_gate_reset_at_given_position

--- tests/test_core.py
+++ tests/test_core.py
@@ -10,12 +10,23 @@ class TestSimulator(unittest.TestCase):
         self.flakiness = 0.01
         self.unit = unit.Simulator(self.flakiness)
 
-    def test_chances_for_no_gate_reset_at_queue_position_0(self):
-        expected_value = self.flakiness
-
-        actual_value = self.unit.chances_for_no_gate_reset_at_any_element_in_queue[0]
-
+    def __chances_for_no_gate_reset_at_given_queue_position(self, queue_position, expected_value):
+        actual_value = self.unit.chances_for_no_gate_reset_at_any_element_in_queue[queue_position]
         self.assertEqual(expected_value, actual_value)
+
+    def test_chances_for_no_gate_reset_at_queue_position_0(self):
+        self.__chances_for_no_gate_reset_at_given_queue_position(0, self.flakiness)
+
+    def test_chances_for_no_gate_reset_at_queue_position_1(self):
+        self.__chances_for_no_gate_reset_at_given_queue_position(1, self.flakiness * self.flakiness)
+
+    def test_chances_for_no_gate_reset_at_queue_position_10(self):
+        self.__chances_for_no_gate_reset_at_given_queue_position(10, pow(self.flakiness,11))
+
+    def test_chances_for_no_gate_reset_at_queue_position_99(self):
+        self.__chances_for_no_gate_reset_at_given_queue_position(99, pow(self.flakiness,100))
+
+
 
 
 if __name__ == '__main__':
