3e412c27d81a2a78d80eb36c6e53c370aab3aad7
Axel Uhlig <axel.uhlig@gmail.com>
2021-02-01 20:36:08 +0000

    plot results

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import matplotlib.pyplot as plt
 from simulator import core
 
 # next steps
-# - plot data
-# - do n times for every flakiness = [0.01, 0.02, 0.03]
 # - do gate resets not at head, but with decreasing probability
 # - write function to search for tipping point in regards of flakiness for given new_elements
 # - plot final results: tipping points on flakiness vs new_elements plot
@@ -15,7 +13,7 @@ def plot_simulation(results, flakiness, new_elements_per_time_interval):
     fig, ax = plt.subplots()
     ax.plot(results)
     ax.set(xlabel='cycle', ylabel='queue length',
-        title='Flakiness: ' + str(flakiness) + ', New elements per cylce: ' + str(new_elements_per_time_interval))
+           title='Flakiness: ' + str(flakiness) + ', New elements per cylce: ' + str(new_elements_per_time_interval))
     ax.grid()
     fig.savefig("test.png")
     plt.show()
@@ -23,10 +21,12 @@ def plot_simulation(results, flakiness, new_elements_per_time_interval):
 
 def main():
     sim = core.Simulator()
-    flakiness = 0.02
+    # if this number is big enough (e.g. 10,000) you  don't need to run the sim n times to get a good result
+    cycle = 10000
+    flakiness = 0.01
     new_elements_per_time_interval = 10
     results = sim.do_basic_sim(flakiness,
-                               new_elements_per_time_interval, cycle=1000, verbosity=False)
+                               new_elements_per_time_interval, cycle, verbosity=False)
     plot_simulation(results, flakiness, new_elements_per_time_interval)
 
 
